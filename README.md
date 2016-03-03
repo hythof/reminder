5f49e7d2602842df43b13a50fb87cc1a64dc93fb
tadokoro <hiroshi.tadokoro@gu3.co.jp>
2016-03-03 23:25:02 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,78 @@
+# why
+一箇所で何でも検索できる何かが欲しい
+
+##「何でもの」定義
+- 後で見たいと思ったもの
+- 自分がオーナーシップを持つものに限る
+
+##「検索項目」の定義
+- フリーワード
+- タグ
+
+##「検索表示内容」の定義
+- 見出し
+- サムネイル(text or image)
+
+##「検索結果の表示順」の定義
+- 評価値順
+
+
+
+# how
+- WebサーバとWebブラウザで実現
+- 更新はstorageを直接触る
+
+browser : chrome
+api     : golang server
+storage : plain text file
+
+(1)インデックス取得
+  browser    api    storage
+  |--------->|      |
+  |          |<---->|
+  |<---------|      |
+1.1 http get
+1.2 file read
+1.3 index json
+
+(2)バイナリ取得
+  browser    api    storage
+  |--------->|      |
+  |          |<---->|
+  |<---------|      |
+1.1 http get
+1.2 file read
+1.3 mp3 / mp4 / pdf
+
+
+
+# who
+- me
+
+
+
+# security
+- スタンドアローンで動作
+
+
+
+# privacy
+- 一部コンテンツはgpgで暗号化
+
+
+
+# testing
+- 単体テスト
+- 負荷テスト(テキストで1G、バイナリで100G)
+
+
+
+# monitoring
+- スタンドアローンのため不要
+
+
+
+# TODO
+[] バイナリの表示対応
+[] Javascriptで全文検索
+[] UX頑張る
--- src/main.go
+++ src/main.go
@@ -1,28 +1,71 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"os"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+)
 
-	"./commands"
+var (
+	port  = 8001
+	texts = []byte(`{"texts":[]}`)
 )
 
 func main() {
-	args := os.Args
-	if len(args) < 2 {
-		usage()
-		return
-	}
-	switch args[1] {
-	case "httpd":
-		commands.Httpd()
-	default:
-		usage()
+	fmt.Printf("listen: 0.0.0.0:%d\n", port)
+	http.HandleFunc("/api/get", routeGet)
+	http.HandleFunc("/", routeIndex)
+
+	loadJson()
+
+	addr := fmt.Sprintf(":%d", port)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Println(err)
+	} else {
+		println("listen", addr)
 	}
 }
 
-func usage() {
-	fmt.Printf(`Usage:
-	%s dev # run httpd server for developer mode
-`, "go run main.go")
+func routeIndex(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path
+	if r.URL.Path == "/" {
+		file = "index.html"
+	}
+	path := fmt.Sprintf("static/%s", file)
+	http.ServeFile(w, r, path)
+}
+
+func routeGet(w http.ResponseWriter, r *http.Request) {
+	loadJson()
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(texts)
+}
+
+func loadJson() error {
+	paths, err := filepath.Glob("backend/**/*.txt")
+	if err != nil {
+		return err
+	}
+	buffers := make([]string, 0, len(paths))
+	capacity := 0
+	for _, path := range paths {
+		item, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		capacity += len(item)
+		buffers = append(buffers, string(item))
+	}
+
+	buf := bytes.NewBuffer(make([]byte, 0, capacity))
+	enc := json.NewEncoder(buf)
+	root := map[string][]string{
+		"texts": buffers,
+	}
+	enc.Encode(root)
+	texts = buf.Bytes()
+	return nil
 }
